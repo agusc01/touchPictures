cd57688de2d2aa38cd9882680be6e59d38896f4d
Agustin <itisanible@gmail.com>
2020-05-08 20:28:48 -0300

    It works!

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
+from PIL import Image
+
 
 def openFile():
 	filesType=(
@@ -14,12 +16,43 @@ def openFile():
 	nameTitle="Open"
 
 	path=filedialog.askopenfilename(title=nameTitle, initialdir=".",filetypes=filesType)
-	print(path)
+	# print(path)
+
+	# ===========================================
+	# ======== ACA OBTENGO EL NOMBRE DEL ARCHIVO
+	# auxPath tiene el string del path invertido
+	"""
+	auxPath=path[::-1]
+	nameFile=""
+	for c in auxPath:
+		if c == '/' or c =='\\':
+			break
+		nameFile+=c
+
+	nameFile=nameFile[::-1]
+	# Aca nameFile tiene el nombre del archivo
+	print(nameFile)
+	"""
+	# ===========================================
+
+	# ===========================================
+	# ======== CREO UN STRING CON .webp
+	fileNewExtend=""
+	for c in path:
+		if c == '.':
+			break
+		fileNewExtend+=c
+	fileNewExtend+=".webp"
+	print(fileNewExtend) 
+	# ===========================================
+
+	imagen = Image.open(path)
+	imagen.save(fileNewExtend)
+
+	print("Done!")
 
 root=Tk()
 root.title("Touch Pictures !")
-root.geometry('350x350')
-
-Button(root,text="Add image",command=openFile).pack()
-
+root.geometry('350x70')
+btn=Button(root,text="Add image",command=openFile).pack()
 root.mainloop()
